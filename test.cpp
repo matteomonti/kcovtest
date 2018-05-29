eb0a2fc9b5fea46ace6f5ffe5d12a33489ebf481
Matteo Monti <matteo.monti@msoftprogramming.com>
2018-05-29 19:02:02 +0000

    Doobidoobi

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std :: cout << "Tu madre!" << std :: endl;
+	int i;
+	std :: cin >> i;
+
+	if(i)
+	{
+		std :: cout << "La sorella della tua bbbbbbaginga!" << std :: endl; std :: cout << "It's a dooby dooobbbby!" << std :: endl;
+	}
+	else
+		std :: cout << "La tu nonna puzza!" << std :: endl;
+}
