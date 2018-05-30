976067341e40be6aba4b6a719fa89389394bb079
Matteo Monti <matteo.monti@msoftprogramming.com>
2018-05-30 22:23:05 +0000

    isdgbj

--- test.cpp
+++ test.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 
-template <typename type> void f()
-{
-	if constexpr (sizeof(type) > 2)
-		std :: cout << "asdasdasd" << std :: endl;
-	else
-		std :: cout << "lmfao" << std :: endl;
-}
-
 int main()
 {
-	f <char> ();
-	f <uint16_t> ();
-
-	std :: cout << "Hello World!!" << std :: endl;
+	int i;
+	std :: cin >> i;
+	if(i)
+		std :: cout << "alpha" << std :: endl;
+	else
+		std :: cout << "beta" << std :: endl;
 }
